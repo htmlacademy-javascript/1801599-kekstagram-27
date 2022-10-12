c250f069f2421640088ddb17f0615a1786c458b5
Alina <alina.melikhova@gmail.com>
2022-10-12 15:41:51 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 function getNumber(numberFrom,numberTo){
-  if(numberFrom < 0 || numberFrom >= numberTo || numberTo < 0){
+  if(numberFrom < 0 || numberTo < 0){
     return NaN;
   }
-  return Math.round(Math.random() * (numberTo - numberFrom) + numberFrom);
+
+  const lower = Math.ceil(Math.min(numberFrom, numberTo));
+  const upper = Math.round(Math.max(numberTo, numberTo));
+
+  return Math.round(Math.random() * (upper - lower) + lower);
 }
 getNumber(20,2);
 
